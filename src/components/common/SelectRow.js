740e26b3dede46f1648b6e4e9844a6d31c0a42cd
alan <saxal28@gmail.com>
2016-12-25 00:22:24 -0600

    inputs are now clear, changed placeholder color to white

--- src/components/common/SelectRow.js
+++ src/components/common/SelectRow.js
@@ -10,9 +10,9 @@ export default function SelectRow(props) {
 
     <div className="col-xs-6 text-left">
     <select className="input-group input-select">
-      <option value={props.value1}>{props.option1}</option>
-      <option value={props.value2}>{props.option2}</option>
-      <option value={props.value3}>{props.option3}</option>
+      <option className="input-select" value={props.value1}>{props.option1}</option>
+      <option className="input-select" value={props.value2}>{props.option2}</option>
+      <option className="input-select" value={props.value3}>{props.option3}</option>
     </select>
     </div>
 
